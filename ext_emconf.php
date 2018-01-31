743ec0173a0336118b6127495d504408ae060d8d
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2018-01-31 15:18:38 +0100

    [BUGFIX] Remove incorrectly declared minimum support (#24)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '2.0.1',
   'constraints' => [
     'depends' => [
-      'php' => '5.6.0-7.2.99',
-      'typo3' => '7.6.0-9.0.99',
+      'php' => '7.0.0-7.2.99',
+      'typo3' => '8.7.0-9.0.99',
     ],
     'conflicts' => [],
     'suggests' => [],
